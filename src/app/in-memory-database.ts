f6b9131cb4926e4c08b423bfe8573c561b026850
Cleiton Luiz de Paula Barbosa <05090722161@jucetins.governo>
2021-01-20 12:22:26 -0300

    Adicionando o serviço 'CategoryService'

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Category } from './pages/categories/shared/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService{
-    createDb(){
-        const categories = [
+    createDb() {
+        const categories: Category[] = [
             { id: 1, nome: 'Moradia', description: 'Pagamento de Contas da Casa'},
             { id: 2, nome: 'Saúde', description: "Plano de Saúde e Remédios"},
             { id: 3, nome: 'Lazer', description: "Cinema, Parques, Praia, etc"},
             { id: 4, nome: 'Salário', description: "Recebimento de Salário"},
-            { id: 5, nome: 'Freelas', description: "Trabalhos com freelancer"},
+            { id: 5, nome: 'Freelas', description: "Trabalhos com freelancer"}
         ];
         return { categories }
     }
