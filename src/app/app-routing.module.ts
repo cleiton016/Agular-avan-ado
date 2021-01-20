47293934707f92f9bd4bf60709a8b177e218fdc3
cleiton016 <cleitonluiz2014@gmail.com>
2021-01-19 22:27:09 -0300

    Implementando o Template do 'CategoryList'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', loadChildren: () => import('src/app/pages/categories/categories.module').then(m => m.CategoriesModule) },
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
