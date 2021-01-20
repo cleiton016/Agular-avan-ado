943cd257224383aa72a3e1ef5a289fc9e00bbac4
Cleiton Luiz de Paula Barbosa <05090722161@jucetins.governo>
2021-01-20 13:49:51 -0300

    Concluindo a implementação do 'CategoryList'

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -5,11 +5,11 @@ import { Category } from './pages/categories/shared/category.model';
 export class InMemoryDatabase implements InMemoryDbService{
     createDb() {
         const categories: Category[] = [
-            { id: 1, nome: 'Moradia', description: 'Pagamento de Contas da Casa'},
-            { id: 2, nome: 'Saúde', description: "Plano de Saúde e Remédios"},
-            { id: 3, nome: 'Lazer', description: "Cinema, Parques, Praia, etc"},
-            { id: 4, nome: 'Salário', description: "Recebimento de Salário"},
-            { id: 5, nome: 'Freelas', description: "Trabalhos com freelancer"}
+            { id: 1, name: 'Moradia', description: 'Pagamento de Contas da Casa'},
+            { id: 2, name: 'Saúde', description: "Plano de Saúde e Remédios"},
+            { id: 3, name: 'Lazer', description: "Cinema, Parques, Praia, etc"},
+            { id: 4, name: 'Salário', description: "Recebimento de Salário"},
+            { id: 5, name: 'Freelas', description: "Trabalhos com freelancer"}
         ];
         return { categories }
     }
